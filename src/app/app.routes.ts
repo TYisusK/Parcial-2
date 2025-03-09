e4dcfe01838ca28ab44a9a13109cb7e379e22029
“TYisusK” <jesussc.ti22@utsjr.edu.mx>
2025-03-09 15:37:03 -0600

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { MenuComponent } from './pages/menu/menu.component';
+import { AcercaComponent } from './pages/acerca/acerca.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+
+export const routes: Routes = [
+    {
+        path: 'acerca_de',
+        component: AcercaComponent
+    },
+    {
+        path: 'home',
+        component: InicioComponent
+    },
+
+    {
+   
+        path: '**',
+        redirectTo: 'home'
+    }
+];
---
+++ src/app/pages/inicio/inicio.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-inicio',
+  imports: [],
+  templateUrl: './inicio.component.html',
+  styleUrl: './inicio.component.css'
+})
+export class InicioComponent {
+  constructor(private router: Router) {}
+
+  // Método para manejar la navegación
+  goTo(route: string): void {
+    this.router.navigate([route]);
+  }
+
+}
